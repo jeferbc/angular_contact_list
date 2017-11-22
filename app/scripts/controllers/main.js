cf14155aa9c3cb5b496ff575631f3648378241cc
jeferbc <jeffe.bernal@gmail.com>
2017-11-22 14:29:19 -0500

    initial import

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularContactListAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularContactListAppApp
+ */
+angular.module('angularContactListAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
